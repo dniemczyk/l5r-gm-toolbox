abff5a3ec8e0ce0582bef06d9fa488f99748d87c
Damian Niemczyk <djniemczyk7@googlemail.com>
2015-02-12 18:20:41 +0100

    Add a very simple test

---
+++ spec/character_spec.rb
@@ -0,0 +1,12 @@
+require 'spec_helper'
+require './lib/character'
+
+RSpec.describe Character do
+
+  describe "With name and age" do
+    it "Should be created" do
+      character = Character.new("Shiba", "Taito", 22)
+      expect(character).not_to be_nil
+    end
+  end
+end
