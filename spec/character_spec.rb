d809294417dbc7991401ef3f22d3ef07bef76d23
Damian Niemczyk <djniemczyk7@googlemail.com>
2016-01-16 14:49:23 +0100

    Clean up whitespace

--- spec/character_spec.rb
+++ spec/character_spec.rb
@@ -2,7 +2,6 @@ require 'spec_helper'
 require './lib/character'
 
 RSpec.describe Character do
-
   describe "With name and age" do
     it "Should be created" do
       character = Character.new("Shiba", "Taito", 22)
