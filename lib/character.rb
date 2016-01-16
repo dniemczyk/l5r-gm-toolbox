7abf67119880c35f572dca355876d9eaac2778b2
Damian Niemczyk <djniemczyk7@googlemail.com>
2016-01-16 23:30:38 +0100

    Create a full name and tests

--- lib/character.rb
+++ lib/character.rb
@@ -2,9 +2,13 @@
 class Character
   attr_reader :family_name, :first_name, :age
 
-  def initialize(family_name, first_name, age)
+  def initialize(first_name, family_name = "", age = 18)
     @family_name = family_name
     @first_name  = first_name
     @age = age
+  end
+
+  def full_name
+    "#{@first_name} #{@family_name}"
   end
 end
--- spec/character_spec.rb
+++ spec/character_spec.rb
@@ -2,10 +2,36 @@ require 'spec_helper'
 require './lib/character'
 
 RSpec.describe Character do
-  describe "With name and age" do
-    it "Should be created" do
-      character = Character.new("Shiba", "Taito", 22)
+  describe "with name and age" do
+    it "should be created" do
+      character = Character.new("Taito", "Shiba", 22)
       expect(character).not_to be_nil
+    end
+  end
+
+  describe "with only first name" do
+    it "should be created with default family name and age" do
+      character = Character.new("Ryu")
+      expect(character).not_to be_nil
+    end
+
+    it "should have a default age of 18" do
+      character = Character.new("Ken")
+      expect(character.age).to eq(18)
+    end
+  end
+
+  describe "#first_name" do
+    it "returns first name" do
+      character = Character.new("Shiba")
+      expect(character.first_name).to eq("Shiba")
+    end
+  end
+
+  describe "#full_name" do
+    it "returns first and family name" do
+      character = Character.new("Taito", "Wakana")
+      expect(character.full_name).to eq("Taito Wakana")
     end
   end
 end
